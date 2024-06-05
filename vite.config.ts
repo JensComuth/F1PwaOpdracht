d877659a3485a28fbf77580cb2ed3aa3fc7a2982
Jens Comuth <131358917+JensComuth@users.noreply.github.com>
2024-06-05 22:53:42 +0200

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -13,6 +12,10 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
   test: {
     globals: true,
